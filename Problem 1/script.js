da2d3fcd73cfa2ddb1d20d2faa2f72f4f55e49a5
LorcanMooney00 <c19335731@mytudublin.ie>
2022-11-21 11:39:10 +0000

    starting
    starting

---
+++ Problem 1/script.js
@@ -0,0 +1,94 @@
+const notesContainer = document.getElementById("app");
+const addNoteButton = notesContainer.querySelector(".add-note");
+
+getNotes().forEach((note) => {
+  const noteElement = createNoteElement(note.id, note.content);
+  notesContainer.insertBefore(noteElement, addNoteButton);
+});
+
+// addNoteButton.addEventListener("click", () => addNote());
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("notes") || "[]");
+}
+
+function saveNotes(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+function createNoteElement(id, content, color) {
+  const element = document.createElement("textarea");
+
+  element.classList.add("note");
+  element.value = content;
+  element.placeholder = "Type notes here";
+
+  element.addEventListener("change", () => {
+    updateNote(id, element.value);
+  });
+
+
+  element.addEventListener("dblclick", () => {
+    const doDelete = confirm(
+      "Are you sure you wish to delete this note?"
+    );
+
+    if (doDelete) {
+      deleteNote(id, element);
+    }
+  });
+
+  if (element.addEventListener) {
+    element.addEventListener('contextmenu', function(e) {
+      alert("change color");
+      element.style.backgroundColor=randomColor()
+      e.preventDefault();
+    }, false);
+  } else {
+    document.attachEvent('oncontextmenu', function() {
+      alert("You've tried to open context menu");
+      window.event.returnValue = false;
+    });
+  }
+
+  return element;
+}
+
+function randomColor() {
+  var myArray = ['red', 'green', 'blue'];
+  const randomElement = myArray[Math.floor(Math.random() * myArray.length)];
+  return randomElement
+}
+
+function addNote() {
+  const notes = getNotes();
+  const noteObject = {
+    id: Math.floor(Math.random() * 1),
+    content: ""
+  };
+
+  const noteElement = createNoteElement(noteObject.id, noteObject.content);
+  notesContainer.insertBefore(noteElement, addNoteButton);
+
+  notes.push(noteObject);
+  saveNotes(notes);
+}
+
+function updateNote(id, newContent) {
+  const notes = getNotes();
+  const targetNote = notes.filter((note) => note.id == id)[0];
+
+  targetNote.content = newContent;
+  saveNotes(notes);
+}
+
+function deleteNote(id, element) {
+  const notes = getNotes().filter((note) => note.id != id);
+
+  saveNotes(notes);
+  notesContainer.removeChild(element);
+}
+
+let addnotes = document.querySelector('notestest');
+rxjs.fromEvent(addNoteButton, 'click').subscribe(() => addNote());
+
