274295485c57fee67c8e6f6f6b7379259334165a
LorcanMooney00 <c19335731@mytudublin.ie>
2022-12-03 04:16:12 +0000

    Got it counting upwards in hours mins and seconds need to reverse the logic and take user input

--- Problem 2/Countdown.js
+++ Problem 2/Countdown.js
@@ -1,6 +1,11 @@
 console.log("hello")
 const { interval, take, map, timer } = rxjs
 //Counting one second in conslog
+const Hours = timer(1000, 7200000);
+const subscribe1 = Hours.subscribe(valueofhours => document.getElementById('hoursDiv').innerHTML=valueofhours);
+
+const Minutes = timer(1000, 120000);
+const subscribe2 = Minutes.subscribe(valueofMinutes => document.getElementById('minutesDiv').innerHTML=valueofMinutes);
 
 const Seconds = timer(1000, 2000);
-const subscribe = Seconds.subscribe(valueofseconds => document.getElementById('secondsDiv').innerHTML=valueofseconds);
+const subscribe3 = Seconds.subscribe(valueofseconds => document.getElementById('secondsDiv').innerHTML=valueofseconds);
