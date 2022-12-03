e71bebf7589c5f35d5695fc0f141ed54405d56b4
LorcanMooney00 <c19335731@mytudublin.ie>
2022-12-03 04:02:34 +0000

    Got it counting up in the HTML gonna get H,M&S now

--- Problem 2/Countdown.js
+++ Problem 2/Countdown.js
@@ -1,5 +1,6 @@
 console.log("hello")
 const { interval, take, map, timer } = rxjs
 //Counting one second in conslog
+
 const Seconds = timer(1000, 2000);
-const subscribe = Seconds.subscribe(valueofseconds => console.log(valueofseconds));
+const subscribe = Seconds.subscribe(valueofseconds => document.getElementById('secondsDiv').innerHTML=valueofseconds);
