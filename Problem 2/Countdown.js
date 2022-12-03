ac44cde34639aa24f97b20980e6d77fbe535c148
LorcanMooney00 <c19335731@mytudublin.ie>
2022-12-03 06:48:02 +0000

    Got stuck did it in js will change to rxjs in morning am tired

--- Problem 2/Countdown.js
+++ Problem 2/Countdown.js
@@ -1,11 +1,46 @@
-console.log("hello")
-const { interval, take, map, timer } = rxjs
-//Counting one second in conslog
-const Hours = timer(1000, 7200000);
-const subscribe1 = Hours.subscribe(valueofhours => document.getElementById('hoursDiv').innerHTML=valueofhours);
 
-const Minutes = timer(1000, 120000);
-const subscribe2 = Minutes.subscribe(valueofMinutes => document.getElementById('minutesDiv').innerHTML=valueofMinutes);
+const { } = rxjs
 
-const Seconds = timer(1000, 2000);
-const subscribe3 = Seconds.subscribe(valueofseconds => document.getElementById('secondsDiv').innerHTML=valueofseconds);
+function hours(){
+  hoursfromhtml=document.getElementById("Hour").value;
+  hoursfromhtml = hoursfromhtml * 3600
+  return hoursfromhtml;
+}
+
+function minutes(){
+  Minsfromhtml=document.getElementById("Minutes").value;
+  Minsfromhtml = Minsfromhtml * 60
+  return Minsfromhtml;
+}
+
+function seconds(){
+  Secsfromhtml=document.getElementById("Seconds").value;
+  return Secsfromhtml;
+}
+
+countdownDiv = document.getElementById("CountdownDiv")
+
+submit.addEventListener('click', event => {
+  var myTimer;
+  let hoursfromhtml = hours();
+  let Minsfromhtml = minutes();
+  let Secsfromhtml = seconds()
+
+  myTimer = setInterval(Countdowntimer, 1000);
+  var TimerDown = Number(hoursfromhtml) + Number(Minsfromhtml) + Number(Secsfromhtml); 
+
+  function Countdowntimer() {
+    TimerDown=TimerDown-1;
+      var seconds = TimerDown % 60; 
+      var secondsInMinutes = (TimerDown - seconds) / 60; 
+      var minutes = secondsInMinutes % 60; 
+      var hours = (secondsInMinutes - minutes) / 60;
+      
+      countdownDiv.innerHTML = `${hours}:${minutes}:${seconds}`
+
+      if (TimerDown == 0) {
+          clearInterval(myTimer);
+          countdownDiv.innerHTML = ""
+      }
+  }
+})
